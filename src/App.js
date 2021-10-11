79b4df333efb51e489e7b389af38ba523d0f7fe8
megan <megan.schertz@gmail.com>
2021-10-11 11:28:33 -0500

    initial fetch request and image rendering

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    cards: []
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/cards')
+    .then(resp => resp.json())
+    .then(data => this.setState({cards: data}))
+    .catch(err => console.log(err))
+  }
+ 
+  render() {
+    console.log(this.state)
+    return (
+      <div>
+        {this.state.cards.map(card =>
+          <div>
+            <h1>{card.name}</h1>
+            <img src={card.image_url.url} />
+          </div>
+          )}
+      </div>
+    )
+  }
 }
 
 export default App;
