097f0d96661e559495e9ed370f6d77b8373e982e
megan <megan.schertz@gmail.com>
2021-10-26 21:34:21 -0500

    draw three cards function working;

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,18 +5,26 @@ import CardImage from "./cards/CardImage"
 class Home extends Component {
     // console.log('home props', props)
     state = {
-        showRandomCard: false
+        showRandomCard: false,
+        showThreeCards: false
     }
 
     getRandomInt(max) {
         return Math.floor(Math.random() * max)
     }
 
-    handleClick = () => {
-        console.log("click random card button")
-
-        this.setState({ showRandomCard: true })
-        console.log(this.state)
+    handleClick = (e) => {
+        debugger
+        if (e.target.value == 'one-card') {
+            console.log("click random card button")
+            
+            this.setState({ showRandomCard: true })
+            console.log(this.state)
+        } else {
+            this.setState({ showThreeCards: true })
+            console.log(this.state)
+        }
+        
     }
 
     drawOneCard = () => {
@@ -27,9 +35,30 @@ class Home extends Component {
         
         return (
             <div>
-                {this.state.showRandomCard? <CardImage card={card} num={num}/> : null }
+                {this.state.showRandomCard ? <CardImage card={card} num={num}/> : null }
             </div>
         )
+    }
+
+    drawThreeCards = () => {
+        const card1 = this.props.cards[Math.floor(Math.random()*this.props.cards.length)]
+        const card2 = this.props.cards[Math.floor(Math.random()*this.props.cards.length)]
+        const card3 = this.props.cards[Math.floor(Math.random()*this.props.cards.length)]
+
+        const cards = [card1, card2, card3]
+
+        if (card1 != card2 && card1 != card3 && card2 != card3) {
+            return (
+                <div>
+                    {this.state.showThreeCards ? 
+                        cards.map(card => <CardImage card={card} num={this.getRandomInt(2)}/> )
+                    : null }
+                </div>
+            )
+        }
+        
+        
+        console.log(cards)
     }
 
     
@@ -42,8 +71,10 @@ class Home extends Component {
                 {/* <h1>{props.user.name}</h1> */}
                 {/* <p>Your cards {props.user.card_ids.map(card => card.id)}</p> */}
                 {/* <CardForm /> */}
-                <button onClick={this.handleClick}>Draw one card</button>
+                <button onClick={this.handleClick} value="one-card">Draw one card</button>
+                <button onClick={this.handleClick}>Draw three cards</button>
                 {this.drawOneCard()}
+                {this.drawThreeCards()}
             </div>
         )
     }
