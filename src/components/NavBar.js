bc31f778b6bf63de36ba057ee4f6eddadb08fbea
megan <megan.schertz@gmail.com>
2021-10-30 17:25:27 -0500

    logout link works

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom"
 import {connect} from "react-redux"
 import { Component } from 'react'
 import { checkAuth } from '../actions/usersActions'
+import { logout } from '../actions/usersActions'
 
 class NavBar extends Component{
 
@@ -11,21 +12,22 @@ class NavBar extends Component{
     }
 
     toggleUserLinks() {
-        const authChecked =this.props.user.authChecked
+        console.log(this.props.user)
         const loggedIn = this.props.user.loggedIn
-        if (authChecked) {
-            return loggedIn ? (   
-                <>
-                    <Link to="/profile" class="nav-link active">Profile</Link>
-                    <Link onClick={this.props.logout} class="nav-link active">Logout</Link>
-                </>
-            ) : (
-                <>
-                    <Link to="/signup" class="nav-link active">Sign Up</Link>
-                    <Link to="/login" class="nav-link active">Login</Link>
-                </>
-            )
-        } 
+
+        return loggedIn ? (   
+            <>
+                <Link to="/profile" class="nav-link active">Profile</Link>
+                <Link to="/logout" onClick={this.props.logout} class="nav-link active">Logout</Link>
+            </>
+        ) : (
+            
+            <>
+                <Link to="/signup" class="nav-link active">Sign Up</Link>
+                <Link to="/login" class="nav-link active">Login</Link>
+            </>
+        )
+        
     }
 
     render() {
@@ -34,10 +36,10 @@ class NavBar extends Component{
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
                     <ul class="navbar-nav mr-auto">
-                            <Link to="/" class="nav-link active">Home</Link>
-                            <Link to="/cards" class="nav-link active">Cards</Link>
-                            <Link to="/arcanas" class="nav-link active">Arcanas</Link>
-                            <Link to="/suits" class="nav-link active">Suits</Link>
+                        <Link to="/" class="nav-link active">Home</Link>
+                        <Link to="/cards" class="nav-link active">Cards</Link>
+                        <Link to="/arcanas" class="nav-link active">Arcanas</Link>
+                        <Link to="/suits" class="nav-link active">Suits</Link>
                     </ul>
                 </div>
                 <div class="navbar navbar-expand-lg navbar-light bg-light">
@@ -61,7 +63,8 @@ const mapStateToProps = (state) => {
   
   const mapDispatchToProps = (dispatch) => {
     return {
-        checkAuth: () => dispatch(checkAuth())
+        checkAuth: () => dispatch(checkAuth()),
+        logout: () => dispatch(logout())
     }
   }
 
